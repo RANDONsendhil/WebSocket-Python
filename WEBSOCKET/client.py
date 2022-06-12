6f0dfbc1d83d852136fa8e39a31be247971bb351
Randon <randon2006@gmail.com>
2022-06-12 10:15:43 +0200

    Push to Repo

---
+++ WEBSOCKET/client.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+import socket
+host, port =('localhost',7000)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    socket.connect((host, port))
+    print("Client connected...")
+    data = "Salut, Coucou, hai..."
+    data = data.encode('utf-8')
+    socket.sendall(data)
+except:
+    print("Connection refused")
+finally:
+    socket.close()
+
---
+++ WEBSOCKET/server.py
@@ -0,0 +1,37 @@
+#coding:utf8
+import socket
+import threading
+
+class clientThread(threading.Thread):
+    def __init__(self, client):
+        threading.Thread.__init__(self)
+        self.client = client
+
+    def run(self):
+        data = conn.recv(1024)
+        data = data.decode('utf8')
+        print(data)
+
+
+host,port = ('',7000)
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind((host,port))
+
+print("Server started...")
+
+while True:
+    socket.listen(5)
+    conn, address = socket.accept()
+    print("Listening...")
+    my_thread = clientThread(conn)
+    my_thread.start()
+
+conn.close()
+socket.close()
+
+
+
+
+
+
