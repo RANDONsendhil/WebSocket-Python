5ff0d2bfe6691a05d31e434bcb048c78b72abb1d
Sendhil RANDON <33903310+RANDONsendhil@users.noreply.github.com>
2022-08-28 22:23:41 +0200

    once more

--- WEBSOCKET/client.py
+++ WEBSOCKET/client.py
@@ -2,6 +2,7 @@
 # Added to git
 # This is for pull request
 # Second pull request from dev to master
+# Third pull request
 import socket
 host, port =('localhost',7000)
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
